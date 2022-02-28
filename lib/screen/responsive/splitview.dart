4d52508bdcc6a046c756ea9e46f51f31b87a6509
Antonino545 <incorvaiaantonino54@gmail.com>
2022-02-28 22:43:51 +0100

    h

---
+++ lib/screen/responsive/splitview.dart
@@ -0,0 +1,46 @@
+// split_view.dart
+import 'package:flutter/material.dart';
+import 'package:hotelmanagement/screen/responsive/responsive.dart';
+
+class SplitView extends StatelessWidget {
+  SplitView({
+    Key? key,
+    // menu and content are now configurable
+    required this.menu,
+    required this.content,
+    // these values are now configurable with sensible default values
+    this.menuWidth = 240,
+  }) : super(key: key);
+  final Widget menu;
+  final Widget content;
+  final double menuWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    if (isTab(context) || isDesktop(context)) {
+      // widescreen: menu on the left, content on the right
+      return Row(
+        children: [
+          SizedBox(
+            width: menuWidth,
+            child: menu,
+          ),
+          Container(width: 0.5, color: Colors.black),
+          Expanded(child: content),
+        ],
+      );
+    } else {
+      // narrow screen: show content, menu inside drawer
+      return Scaffold(
+        body: content,
+        drawer: SizedBox(
+          width: menuWidth,
+          child: Drawer(
+            child: menu,
+          ),
+        ),
+      );
+    }
+  }
+}
