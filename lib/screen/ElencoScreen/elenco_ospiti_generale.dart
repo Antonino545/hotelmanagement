e70fbfb389c0fee1693455e0c7b1fcbd6ec8fafe
Antonino545 <incorvaiaantonino54@gmail.com>
2022-03-19 11:53:04 +0100

    slidable update

--- lib/screen/ElencoScreen/elenco_ospiti_generale.dart
+++ lib/screen/ElencoScreen/elenco_ospiti_generale.dart
@@ -1,9 +1,13 @@
 // ignore_for_file: must_be_immutable
 
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:hotelmanagement/app.dart';
 import 'package:hotelmanagement/components/generalfunctions.dart';
 import 'package:hotelmanagement/screen/AggiungiScreen/aggiungi_prenotazione_screen.dart';
@@ -47,48 +51,60 @@ class _ElencoOspitiGeneraliState extends State<ElencoOspitiGenerali> {
                         itemBuilder: (context, index) {
                           DocumentSnapshot documentSnapshot =
                               snapshots.data!.docs[index];
-                          return Dismissible(
-                            onDismissed: (direction) {
-                              try {
-                                FirebaseFirestore.instance
-                                    .collection('Dati')
-                                    .doc(user?.uid)
-                                    .collection("prenotazioni")
-                                    .doc(documentSnapshot.id)
-                                    .delete();
-                                int ospitiindex = 0;
-                                DocumentSnapshot ospiticounter =
-                                    snapshots.data!.docs[ospitiindex];
-                                if (kDebugMode) {
-                                  print(ospiticounter);
-                                }
-                                /*FirebaseFirestore.instance
-                                                .collection('Dati')
-                                                .doc(user?.uid)
-                                                .collection("prenotazioni")
-                                                .doc(cognomePrenotazione)
-                                                .collection("Ospiti")
-                                                .doc(documentSnapshot.id)
-                                                .delete();*/
-                              } catch (e) {
-                                if (kDebugMode) {
-                                  print(e);
-                                }
-                              }
-                            },
+                          return Slidable(
                             key: ObjectKey(documentSnapshot.data()),
-                            background: Card(
-                              color: Colors.red,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.delete,
-                                    size: 30,
-                                  ),
-                                ],
+                            startActionPane: ActionPane(
+                              // A motion is a widget used to control how the pane animates.
+                              motion: const BehindMotion(),
+
+                              // A pane can dismiss the Slidable.
+                              dismissible: DismissiblePane(
+                                onDismissed: () async {
+                                  await alertTwoButton(
+                                      context,
+                                      "Sei sicuro di cancellare questa prenotazione",
+                                      user,
+                                      documentSnapshot);
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              SplitView()));
+                                },
                               ),
+
+                              // All actions are defined in the children parameter.
+                              children: [
+                                // A SlidableAction can have an icon and/or a label.
+                                SlidableAction(
+                                  onPressed: (direction) async {
+                                    await alertTwoButton(
+                                        context,
+                                        "Sei sicuro di cancellare questa prenotazione",
+                                        user,
+                                        documentSnapshot);
+                                    Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (BuildContext context) =>
+                                                SplitView()));
+                                  },
+                                  backgroundColor: Color(0xFFFE4A49),
+                                  foregroundColor: Colors.white,
+                                  icon: Icons.delete,
+                                ),
+                                SlidableAction(
+                                    onPressed: (direction) {
+                                      alertTwoButton(
+                                          context,
+                                          "Sei sicuro di cancellare questa prenotazione",
+                                          user,
+                                          documentSnapshot);
+                                    },
+                                    backgroundColor: Colors.teal,
+                                    foregroundColor: Colors.white,
+                                    icon: Icons.edit),
+                              ],
                             ),
                             child: Card(
                                 child: Column(children: [
@@ -153,6 +169,81 @@ class _ElencoOspitiGeneraliState extends State<ElencoOspitiGenerali> {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+Future<void> alertTwoButton(BuildContext context, box, user, documentSnapshot) {
+  if (Platform.isIOS) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          content: Text(box),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: const Text('Si'),
+              onPressed: () {
+                try {
+                  FirebaseFirestore.instance
+                      .collection('Dati')
+                      .doc(user?.uid)
+                      .collection("prenotazioni")
+                      .doc(documentSnapshot.id)
+                      .delete();
+                } catch (e) {
+                  if (kDebugMode) {
+                    print(e);
+                  }
+                }
+              },
+            ),
+            CupertinoDialogAction(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } else {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Text(box),
+          actions: <Widget>[
+            Row(
+              children: [
+                TextButton(
+                  child: const Text('Si'),
+                  onPressed: () {
+                    try {
+                      FirebaseFirestore.instance
+                          .collection('Dati')
+                          .doc(user?.uid)
+                          .collection("prenotazioni")
+                          .doc(documentSnapshot.id)
+                          .delete();
+                    } catch (e) {
+                      if (kDebugMode) {
+                        print(e);
+                      }
+                    }
+                  },
+                ),
+                TextButton(
+                    child: const Text('No'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    }),
+              ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
